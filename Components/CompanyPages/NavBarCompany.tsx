3796c3f8eb3eca6ea13af2d06af46d3b3ecb8e0a
Apostolis2002 <apostolissiabanis@hotmail.com>
2023-06-06 14:49:20 +0300

    remove Create job position button from other pages rather than home page

--- Components/CompanyPages/NavBarCompany.tsx
+++ Components/CompanyPages/NavBarCompany.tsx
@@ -34,13 +34,13 @@ export default function NavBarCompany(props: any) {
 
                 <div className="flex md:order-2">
 
-                    <div className="md:flex hidden list-none">
+                    {pathname === '/company/home' && <div className="md:flex hidden list-none">
                         <Navbar.Link href="/company/create-job-position" onClick={() => console.log()}>
                             <div className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
                                 Create job position
                             </div>
                         </Navbar.Link>
-                    </div>
+                    </div>}
 
                     {pathname === '/company/home' ? <button type="button" onClick={() => props.setShowFilters(true)} className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Filters</button> : null}
                     {<div className="flex md:hidden">
