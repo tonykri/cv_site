9372938ad7a91bce71886302b011ce940842286d
Apostolis2002 <apostolissiabanis@hotmail.com>
2023-06-17 12:13:49 +0300

    create job position add label

--- app/company/create-job-position/page.tsx
+++ app/company/create-job-position/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <NavBarCompany/>
+      <h1 className="text-4xl text-center">Create Job Position</h1>
       <CreateJobForm/>
       {/* Footer */} 
       <footer className="text-center p-2 dark:text-white">
